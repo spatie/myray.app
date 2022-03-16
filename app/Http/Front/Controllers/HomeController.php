3115b0f5cab98b42e01a5a5b53608deae06fb985
Freek Van der Herten <freek@spatie.be>
2022-03-16 11:20:19 +0100

    wip

--- app/Http/Front/Controllers/HomeController.php
+++ app/Http/Front/Controllers/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Front\Controllers;
 
+use App\Support\Testimonial;
 use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 use Spatie\PriceApi\SpatiePriceApi;
 
 class HomeController
@@ -24,6 +26,44 @@ class HomeController
             'price' => $prices['actual'],
             'priceWithoutDiscount' => $prices['withoutDiscount'],
             'discount' => $prices['discount'],
+            'testimonials' => $this->getTestimonials()
+        ]);
+    }
+
+    protected function getTestimonials(): Collection
+    {
+        return collect([
+            new Testimonial(
+                name: 'Taylor Otwell',
+                text: 'Ray is <strong class=" font-black text-2xl block">Life</strong>',
+                image: 'taylor',
+                url: 'https://twitter.com/taylorotwell',
+                title: 'Laravel Founder & Creator',
+            ),
+            new Testimonial(
+                name: 'Michael Dyrynda',
+                text: 'As an amateur developer that swears by <code>dd()</code>, I was thrilled to hear about Ray. Now
+    I can <strong class="font-semibold">feel like a real developer</strong>, even without using \'real\'
+    debugging tools!',
+                image: 'michael',
+                url: 'https://twitter.com/michaeldyrynda',
+                title: 'Senior Developer at MaxoTel',
+            ),
+            new Testimonial(
+                name: 'Nuno Maduro',
+                text: 'Ray is a part of my <strong class="font-semibold">Essentials</strong> toolbox. It has the
+        snapiness of a real debugger, but the simplicity of <code>dd()</code>',
+                image: 'nuno',
+                url: 'https://twitter.com/enunomaduro',
+                title: 'Software engineer at Laravel',
+            ),
+            new Testimonial(
+                name: 'James Brooks',
+                text: 'Ray is the app I\'ve been missing in my development toolkit. Debugging any sized application is now a breeze.',
+                image: 'james',
+                url: 'https://twitter.com/jbrooksuk',
+                title: 'Software engineer at Laravel',
+            ),
         ]);
     }
 }
