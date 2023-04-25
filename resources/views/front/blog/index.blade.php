f8bde9fffe92e21d1d688cfc89920f48de562a30
Freek Van der Herten <freek@spatie.be>
2023-04-25 16:26:34 +0200

    wip

--- resources/views/front/blog/index.blade.php
+++ resources/views/front/blog/index.blade.php
@@ -6,32 +6,48 @@
 @section('content')
     <main>
         <div class="mx-auto px-6 sm:px-12 md:px-16 pb-16 max-w-4xl">
-            @foreach($posts as $post)
-                <div class="mt-8">
-                    <h2 class="hover:underline">
-                        <a href="{{ route('post.show', $post->idSlug()) }}">
-                            {{ $post->title }}
+            @if(count($posts))
+                @foreach($posts as $post)
+                    <div class="mt-8">
+                        <h2 class="hover:underline">
+                            <a href="{{ route('post.show', $post->idSlug()) }}">
+                                {{ $post->title }}
+                            </a>
+                        </h2>
+                        <div class="text-indigo-900 text-opacity-50 text-xs">
+                            {{ $post->published_at->format('d F Y') }}
+                        </div>
+                        <p class="text-indigo-900 text-opacity-50 mt-2 text-sm">
+                            {{ $post->excerpt }}
+                        </p>
+                        <a
+                            class="underline text-xs mt-4"
+                            href="{{ route('post.show', $post->idSlug()) }}">
+                            Read more
                         </a>
-                    </h2>
-                    <div class="text-indigo-900 text-opacity-50 text-xs">
-                        {{ $post->published_at->format('d F Y') }}
                     </div>
-                    <p class="text-indigo-900 text-opacity-50 mt-2 text-sm">
-                        {{ $post->excerpt }}
-                    </p>
-                    <a
-                        class="underline text-xs mt-4"
-                        href="{{ route('post.show', $post->idSlug()) }}">
-                        Read more
-                    </a>
+                @endforeach
+                <div class="mt-8">
+                    {{ $posts->links() }}
                 </div>
-            @endforeach
-            <div class="mt-8">
-            {{ $posts->links() }}
+
+                @include('partials.cta')
+
+            @endif
+
+            <div class="mt-12 justify-center text-xl">
+                <div>
+                    The first post will be published soon...
+                </div>
+
+                <div>
+                <a
+                    class="underline text-xs mt-4"
+                    href="/">
+                    Go back
+                </a>
+                </div>
             </div>
-
-            @include('partials.cta')
-
         </div>
     </main>
 @endsection
