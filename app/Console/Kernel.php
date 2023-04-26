c34e20690a565b380fa28ec486924062da7266c1
Freek Van der Herten <freek@spatie.be>
2023-04-26 10:27:13 +0200

    wip

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\PublishScheduledPostsCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -9,6 +10,7 @@ class Kernel extends ConsoleKernel
 {
     protected function schedule(Schedule $schedule): void
     {
+        $schedule->command(PublishScheduledPostsCommand::class)->everyMinute();
     }
 
     protected function commands(): void
