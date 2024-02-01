d498b7a8b2ebc8dbe02a32ccb7ccebb580b3b605
Tim Van Dijck <tim.van.dijck@gmail.com>
2024-02-01 16:25:10 +0100

    Fix sorting.

--- app/Domain/Docs/Models/Category.php
+++ app/Domain/Docs/Models/Category.php
@@ -2,35 +2,37 @@
 
 namespace App\Domain\Docs\Models;
 
+use Illuminate\Support\Collection;
+
 class Category
 {
-    // @var array<string, Category>
-    public array $subCategories = [];
+    // @var Collection<string, Category>
+    public Collection $subCategories;
 
-    public array $pages = [];
+    public Collection $pages;
 
     public int $weight = 99;
 
     public string $title = '<no title>';
 
+    public function __construct() {
+        $this->subCategories = new Collection();
+        $this->pages = new Collection();
+    }
+
     public function childCategory(string $key): Category
     {
-        if (! isset($this->subCategories[$key])) {
-            $this->subCategories[$key] = new Category($key);
+        if (! $this->subCategories->has($key)) {
+            $this->subCategories->put($key, new Category());
         }
 
-        return $this->subCategories[$key];
+        return $this->subCategories->get($key);
     }
 
     public function sortCategories(): void
     {
-        uasort($this->subCategories, function ($a, $b) {
-            return $a->weight <=> $b->weight;
-        });
-
-        usort($this->pages, function ($a, $b) {
-            return $a->weight <=> $b->weight;
-        });
+        $this->subCategories = $this->subCategories->sortBy('weight');
+        $this->pages = $this->pages->sortBy('weight');
 
         foreach ($this->subCategories as $subCategory) {
             $subCategory->sortCategories();
