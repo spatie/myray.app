1511372c3ed9a2ea6e6cbf9541c3505bfd9427ef
Tim Van Dijck <tim.van.dijck@gmail.com>
2024-02-08 15:19:40 +0100

    Improved docs menu

--- resources/views/partials/docs/navigation-category.blade.php
+++ resources/views/partials/docs/navigation-category.blade.php
@@ -1,25 +1,29 @@
-<div class="navigation-category" x-data="{ open: {{\Illuminate\Support\Str::contains($slug, $category->slug) ? 'true' : 'false'}} }">
-    <strong class="cursor-pointer text-base font-semibold" @click="open = !open">{{$category->title}}</strong>
-    <ul x-show="open">
-        @foreach($category->subCategories as $subCategory)
-            <li>
-                @include('partials.docs.navigation-sub-category', ['category' => $subCategory])
-            </li>
-        @endforeach
-        @foreach($category->pages as $page)
-            <li
-                @class([
-                    'active border-indigo-500' => \Illuminate\Support\Str::contains($slug, $page->slug),
-                ])
-            >
-                <a href="{{$page->url}}"
+<div class="navigation-category">
+    <a href="{{$category->firstPage()->url}}" class="pl-0 cursor-pointer text-base font-semibold">
+        {{$category->title}}
+    </a>
+    @if(\Illuminate\Support\Str::contains($slug, $category->slug))
+        <ul>
+            @foreach($category->subCategories as $subCategory)
+                <li>
+                    @include('partials.docs.navigation-sub-category', ['category' => $subCategory])
+                </li>
+            @endforeach
+            @foreach($category->pages as $page)
+                <li
                     @class([
-                        'active border-indigo-500 border-s' => \Illuminate\Support\Str::contains($slug, $page->slug),
+                        'active border-indigo-500' => \Illuminate\Support\Str::contains($slug, $page->slug),
                     ])
                 >
-                    {{$page->title}}
-                </a>
-            </li>
-        @endforeach
-    </ul>
+                    <a href="{{$page->url}}"
+                        @class([
+                            'active border-indigo-500 border-s' => \Illuminate\Support\Str::contains($slug, $page->slug),
+                        ])
+                    >
+                        {{$page->title}}
+                    </a>
+                </li>
+            @endforeach
+        </ul>
+    @endif
 </div>
--- resources/views/partials/docs/navigation-sub-category.blade.php
+++ resources/views/partials/docs/navigation-sub-category.blade.php
@@ -1,19 +1,25 @@
-<strong @class([
-    '-left-px active border-l border-indigo-500' => \Illuminate\Support\Str::contains($slug, $category->slug),
+<a href="{{$category->firstPage()->url}}" @class([
+    '-left-px active border-l border-indigo-500 font-semibold text-indigo-500' => \Illuminate\Support\Str::contains($slug, $category->slug),
 ])>
     {{$category->title}}
-</strong>
-<ul>
-    @foreach($category->subCategories as $subCategory)
-        <li>
-            @include('partials.docs.navigation-sub-category', ['category' => $subCategory])
-        </li>
-    @endforeach
-    @foreach($category->pages as $page)
-        <li>
-            <a href="{{$page->url}}">
-                {{$page->title}}
-            </a>
-        </li>
-    @endforeach
-</ul>
+</a>
+@if(\Illuminate\Support\Str::contains($slug, $category->slug))
+    <ul>
+        @foreach($category->subCategories as $subCategory)
+            <li>
+                @include('partials.docs.navigation-sub-category', ['category' => $subCategory])
+            </li>
+        @endforeach
+        @foreach($category->pages as $page)
+            <li>
+                <a href="{{$page->url}}"
+                    @class([
+                        'active text-indigo-500' => \Illuminate\Support\Str::contains($slug, $page->slug),
+                    ])
+                >
+                    {{$page->title}}
+                </a>
+            </li>
+        @endforeach
+    </ul>
+@endif
