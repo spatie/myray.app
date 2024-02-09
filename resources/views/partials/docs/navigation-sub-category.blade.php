071b22827f20169a85343e5e047b308cf529d0f0
Tim Van Dijck <tim.van.dijck@gmail.com>
2024-02-09 13:55:06 +0100

    Improved docs menu.

--- resources/views/partials/docs/navigation-sub-category.blade.php
+++ resources/views/partials/docs/navigation-sub-category.blade.php
@@ -1,25 +1,30 @@
-<a href="{{$category->firstPage()->url}}" @class([
-    '-left-px active border-l border-indigo-500 font-semibold text-indigo-500' => \Illuminate\Support\Str::contains($slug, $category->slug),
-])>
+<a href="{{$category->firstPage()->url}}"
+    @class([
+        '-left-px active border-l border-indigo-500 font-semibold text-indigo-500' => \Illuminate\Support\Str::contains($slug, $category->slug),
+    ])
+>
     {{$category->title}}
 </a>
 @if(\Illuminate\Support\Str::contains($slug, $category->slug))
     <ul>
-        @foreach($category->categories as $subCategory)
-            <li>
-                @include('partials.docs.navigation-sub-category', ['category' => $subCategory])
-            </li>
-        @endforeach
-        @foreach($category->pages as $page)
-            <li>
-                <a href="{{$page->url}}"
-                    @class([
-                        'active text-indigo-500' => \Illuminate\Support\Str::contains($slug, $page->slug),
-                    ])
-                >
-                    {{$page->title}}
-                </a>
-            </li>
-        @endforeach
+
+            @foreach($category->categories as $subCategory)
+                <li>
+                    @include('partials.docs.navigation-sub-category', ['category' => $subCategory])
+                </li>
+            @endforeach
+            @if(count($category->pages) >= 2)
+                @foreach($category->pages as $page)
+                    <li>
+                        <a href="{{$page->url}}"
+                            @class([
+                                'active text-indigo-500' => \Illuminate\Support\Str::contains($slug, $page->slug),
+                            ])
+                        >
+                            {{$page->title}}
+                        </a>
+                    </li>
+                @endforeach
+            @endif
     </ul>
 @endif
