882cce61f78c27ca55bd78159e51be9cb2aca4dd
Tim Van Dijck <tim.van.dijck@gmail.com>
2024-02-19 16:48:39 +0100

    Always show expanded menu.

--- resources/views/partials/docs/navigation-category.blade.php
+++ resources/views/partials/docs/navigation-category.blade.php
@@ -2,28 +2,26 @@
     <a href="{{$category->firstPage()->url}}" class="pl-0 cursor-pointer text-sm font-semibold">
         {{$category->title}}
     </a>
-    @if(\Illuminate\Support\Str::contains($slug, $category->slug))
-        <ul>
-            @foreach($category->categories as $subCategory)
-                <li>
-                    @include('partials.docs.navigation-sub-category', ['category' => $subCategory])
-                </li>
-            @endforeach
-            @foreach($category->pages as $page)
-                <li
+    <ul>
+        @foreach($category->categories as $subCategory)
+            <li>
+                @include('partials.docs.navigation-sub-category', ['category' => $subCategory])
+            </li>
+        @endforeach
+        @foreach($category->pages as $page)
+            <li
+                @class([
+                    'active border-indigo-500' => \Illuminate\Support\Str::contains($slug, $page->slug),
+                ])
+            >
+                <a href="{{$page->url}}"
                     @class([
-                        'active border-indigo-500' => \Illuminate\Support\Str::contains($slug, $page->slug),
+                        'active border-indigo-500 border-s' => \Illuminate\Support\Str::contains($slug, $page->slug),
                     ])
                 >
-                    <a href="{{$page->url}}"
-                        @class([
-                            'active border-indigo-500 border-s' => \Illuminate\Support\Str::contains($slug, $page->slug),
-                        ])
-                    >
-                        {{$page->menuTitle()}}
-                    </a>
-                </li>
-            @endforeach
-        </ul>
-    @endif
+                    {{$page->menuTitle()}}
+                </a>
+            </li>
+        @endforeach
+    </ul>
 </div>
