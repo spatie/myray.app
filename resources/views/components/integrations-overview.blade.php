9c1071faf99c206e1ad6288ad3f06119958240ca
Tim Van Dijck <tim.van.dijck@gmail.com>
2024-02-19 17:15:36 +0100

    Fix.

--- resources/views/components/integrations-overview.blade.php
+++ resources/views/components/integrations-overview.blade.php
@@ -2,7 +2,7 @@
     @foreach($docs as $doc)
         <a href="{{$doc->url}}" class="text-center">
             <div class="border rounded p-5 flex align-items-center justify-center">
-                <img class="w-3/5" src="{{Vite::asset('resources/images/logos/'.$doc->parts[1].'.png')}}" alt="{{$doc->title}}">
+                <img class="w-3/5" src="https://myray.app/images/logos/{{$doc->parts[1]}}.png" alt="{{$doc->title}}">
             </div>
 
             <strong class="font-semibold text-sm mt-1 inline-block">{{$doc->title}}</strong>
