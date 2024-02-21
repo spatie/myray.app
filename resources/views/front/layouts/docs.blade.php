5046e3ad9b524ff0f4950ce20b5d181e2b5fc69d
sebastiandedeyne <sebastiandedeyne@gmail.com>
2024-02-21 12:41:11 +0100

    Add wire:navigate to docs

--- resources/views/front/layouts/docs.blade.php
+++ resources/views/front/layouts/docs.blade.php
@@ -4,7 +4,7 @@
     @include('partials.layout.head')
 </head>
 <body class="bg-white font-sans font-medium text-black docs">
-    <div class="border-b border-gray-200 bg-white relative z-50">
+    <div class="border-b border-gray-200 bg-white relative z-50" id="header">
         @include('partials.header')
     </div>
 
@@ -14,21 +14,66 @@
         </div>
 
         <div class="lg:flex">
-            <div class="border-r border-gray-200">
-                <x-docs-navigation></x-docs-navigation>
-            </div>
+            @persist('scrollbar')
+                <div class="border-r border-gray-200 sticky top-0 lg:h-screen overflow-y-scroll" wire:scroll>
+                    <x-docs-navigation></x-docs-navigation>
+                </div>
+            @endpersist
 
-            <section class="page-content lg:w-full p-5 lg:p-14">
+            <section class="page-content lg:w-full p-5 lg:p-14 lg:pb-24">
                 @yield('content')
-            </section>
-        </div>
 
-        <div class="absolute right-0 -bottom-4">
-            @include('partials.docs.ornament')
+                <div class="absolute right-0 bottom-24">
+                    @include('partials.docs.ornament')
+                </div>
+            </section>
         </div>
     </div>
 
-    @include('partials.footer')
+    <script>
+        document.addEventListener('alpine:init', () => {
+            let state = Alpine.reactive({ path: window.location.pathname });
+
+            function strip(path) {
+                parts = path.replace(/https?:\/\//, '').split('/');
+                parts.shift();
+
+                return parts.join('/');
+            }
+
+            function section(path) {
+                parts = path.split('/');
+                parts.pop();
+
+                return parts.join('/');
+            }
+
+            let scrollPosition = 0;
+
+            document.addEventListener('livewire:navigating', () => {
+                scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+            });
+
+            document.addEventListener('livewire:navigated', () => {
+                queueMicrotask(() => {
+                    document.documentElement.scrollTop = document.body.scrollTop = Math.min(
+                        scrollPosition,
+                        document.getElementById('header').clientHeight,
+                    );
+
+                    state.path = window.location.pathname;
+                });
+            });
+
+            Alpine.magic('current', (el) => (expected = '') => {
+                return strip(state.path) === strip(expected);
+            });
+
+            Alpine.magic('currentSection', (el) => (expected = '') => {
+                return section(strip(state.path)) === section(strip(expected));
+            });
+        });
+    </script>
 
     @include('partials.layout.scripts')
 </body>
