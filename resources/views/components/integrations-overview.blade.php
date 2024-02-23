ac64666e9201740b37aff39d3b4b80443bdc31ca
Tim Van Dijck <tim.van.dijck@gmail.com>
2024-02-23 09:35:56 +0100

    Improve integrations overview.

--- resources/views/components/integrations-overview.blade.php
+++ resources/views/components/integrations-overview.blade.php
@@ -1,11 +1,11 @@
 <div class="grid grid-cols-3 gap-4">
     @foreach($docs as $doc)
-        <a href="{{$doc->url}}" class="text-center">
-            <div class="border rounded custom-shadow p-5 flex align-items-center justify-center">
+        <a href="{{$doc->url}}" class="text-center mb-1">
+            <div class="border rounded custom-shadow p-4 flex align-items-center justify-center">
                 <img class="w-3/5" src="https://myray.app/images/logos/{{$doc->parts[1]}}.png" alt="{{$doc->title}}">
             </div>
 
-            <strong class="font-semibold text-sm mt-1 inline-block">{{$doc->title}}</strong>
+            <strong class="font-semibold text-xs text-black font-semibold mt-2 inline-block">{{$doc->title}}</strong>
         </a>
     @endforeach
 </div>
