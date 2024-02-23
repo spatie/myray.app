8c6a1ba90f5788a7645dfb309234ca87ff44d740
sebastiandedeyne <sebastiandedeyne@gmail.com>
2024-02-23 17:33:48 +0100

    CS

--- resources/views/livewire/doc-search.blade.php
+++ resources/views/livewire/doc-search.blade.php
@@ -1,21 +1,29 @@
 @persist('search')
 <div>
-
-
-    <div class="fixed left-0 right-0 top-0 bottom-0 bg-indigo-100/75 z-40" x-cloak x-show="showSearchBox" @click="showSearchBox=false">
-    </div>
-
-    <div class="fixed left-0 right-0 top-0 bottom-0 z-50 pointer-events-none" x-cloak x-show="showSearchBox" x-trap="showSearchBox">
+    <div
+        class="fixed left-0 right-0 top-0 bottom-0 bg-indigo-100/75 z-40"
+        x-cloak
+        x-show="showSearchBox"
+        @click="showSearchBox=false"
+    ></div>
+    <div
+        class="fixed left-0 right-0 top-0 bottom-0 z-50 pointer-events-none"
+        x-cloak
+        x-show="showSearchBox"
+        x-trap="showSearchBox"
+    >
         <div class="flex justify-center items-center h-full">
             <div class="bg-white shadow-lg w-full max-w-2xl rounded pointer-events-auto m-3">
                 <div class="relative flex items-center border-b">
                     <div class="absolute left-0 z-50 left-3">
                         @include('partials.svg.icon-search')
                     </div>
-
-                    <input class="w-full p-3 pl-10 outline-none rounded" type="text" wire:model.live.debounce="query">
+                    <input
+                        type="text"
+                        class="w-full p-3 pl-10 outline-none rounded"
+                        wire:model.live.debounce="query"
+                    >
                 </div>
-
                 @if($results)
                     <div class="max-h-80 overflow-y-auto px-3 pb-3">
                         <div class="text-sm font-bold mt-2 mb-2">Results</div>
