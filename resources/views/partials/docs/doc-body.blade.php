1fd95a4e972744e90ab538a8b05b6ffcae11a0e7
Tim Van Dijck <tim.van.dijck@gmail.com>
2024-02-23 16:22:56 +0100

    Add github link to pages.

--- resources/views/partials/docs/doc-body.blade.php
+++ resources/views/partials/docs/doc-body.blade.php
@@ -14,3 +14,10 @@
 </div>
     </x-markdown>
 </section>
+
+<a class="text-xs text-midnightDark mt-5 block flex items-center" target="_blank" href="https://github.com/spatie/myray.app/blob/main/docs/{{$page->getPath()}}">
+    <div class="mr-2 inline-block">
+        @include('partials.svg.icon-github')
+    </div>
+    Help us improve this page
+</a>
