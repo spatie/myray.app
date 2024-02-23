428b60bf4fa2ba7fab0ff908bd28b72406433a77
Tim Van Dijck <tim.van.dijck@gmail.com>
2024-02-23 09:27:32 +0100

    Shadow refactor.

--- resources/views/components/integrations-overview.blade.php
+++ resources/views/components/integrations-overview.blade.php
@@ -1,7 +1,7 @@
 <div class="grid grid-cols-3 gap-4">
     @foreach($docs as $doc)
         <a href="{{$doc->url}}" class="text-center">
-            <div class="border rounded p-5 flex align-items-center justify-center">
+            <div class="border rounded custom-shadow p-5 flex align-items-center justify-center">
                 <img class="w-3/5" src="https://myray.app/images/logos/{{$doc->parts[1]}}.png" alt="{{$doc->title}}">
             </div>
 
