006c0b65d6994d62b04d24efb159fee033ddc738
Jimi Robaer <jimi.robaer@hey.com>
2024-07-23 15:59:21 +0200

    Simple styling for 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,11 +1,22 @@
 <x-layouts.default title="Page not found">
-    <div class="text-2xl">
-        <p>
-            Aaaw… <br>
-            We couldn't find this page.
-        </p>
-        <p class="mt-8">
-            Let's shine some light and get you back to <a class="markup-link" href="/">the homepage</a>.
-        </p>
+
+    <div class="container max-w-4xl mx-auto pb-12 md:pb-0">
+        <div class="mx-auto px-6 sm:px-12 md:px-16">
+
+            <div class="p-8 text-center">
+                <h1 class="font-display font-black text-5xl tracking-tight mb-[0.35em] md:text-7xl">
+                    <span class="bg-gradient-to-r from-orange to-bright-orange text-transparent bg-clip-text">
+                        Page not found
+                    </span>
+                </h1>
+
+                <div class="markup text-2xl">
+                    <p> Sorry, nothing to see here.<br />
+                    <a href="/">Teleport yourself</a> to the homepage instead.</p>
+                </div>
+            </div>
+            
+        </div>
     </div>
+
 </x-layouts.default>
