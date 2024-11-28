5d1cad872149462078d81bdc7a4755542f0ddb45
Rias <hey@rias.be>
2024-11-28 16:53:45 +0100

    Enable lifetime cta

--- resources/views/partials/priceCard.blade.php
+++ resources/views/partials/priceCard.blade.php
@@ -5,36 +5,6 @@
 <x-download />
 
 <div class="mt-4 mb-4 mx-auto text-sm text-indigo-900 text-opacity-50">
-    {{--
-        <a href="{{spatieUrl('https://spatie.be/products/ray')}}">
-            <button class="group
-        py-6 px-6 w-full
-        bg-gradient-to-r from-indigo-800 to-indigo-700
-        border-b border-r border-orange-900
-        shadow-lg rounded-sm
-        font-normal text-white text-xl
-        transform active:translate-y-px
-        focus:outline-none focus:ring-0
-        whitespace-nowrap
-        overflow-hidden">
-                <div class="text-sm">⚡️ Special Offer</div>
-                Get a <strong>lifetime</strong> license!
-            </button>
-        </a>
-        <div class="mt-3 mb-10 text-xs text-center text-indigo-900 text-opacity-50">
-            Available for
-            @php
-                $expirationDate = \Carbon\Carbon::createFromFormat('Y-m-d H:i', '2022-11-29 09:00' );
-            @endphp
-            <x-countdown class="inline-block tabular-nums font-bold" :expires="$expirationDate">
-                <span x-text="timer.days">{{ $component->days() }}</span>d</span>
-                <span x-text="timer.hours">{{ $component->hours() }}</span>h</span>
-                <span x-text="timer.minutes">{{ $component->minutes() }}</span>m</span>
-                <span x-text="timer.seconds">{{ $component->seconds() }}</span>s</span>
-            </x-countdown>
-        </div>
-    --}}
-
     <a class="group inline-flex items-center" target="_blank" href="{{spatieUrl('https://spatie.be/products/ray')}}">
         <span class="leading-tight border-b border-indigo-900 border-opacity-50">
             Get an annual license
@@ -56,8 +26,36 @@
             transform group-hover:scale-90
             transition-transform duration-300">{{ $price->formattedPrice() }}</span>
         @endif
-
     </a>
+    @if (\Illuminate\Support\Facades\Date::create(2024, 11, 28)->isPast() && \Illuminate\Support\Facades\Date::create(2024, 11, 30)->isFuture())
+        <a href="{{spatieUrl('https://spatie.be/products/ray')}}">
+            <button class="group mt-6
+        py-6 px-6 w-full
+        bg-gradient-to-r from-indigo-800 to-indigo-700
+        border-b border-r border-orange-900
+        shadow-lg rounded-sm
+        font-normal text-white text-xl
+        transform active:translate-y-px
+        focus:outline-none focus:ring-0
+        whitespace-nowrap
+        overflow-hidden">
+                <div class="text-sm">⚡️ Special Offer</div>
+                Get a <strong>lifetime</strong> license!
+            </button>
+        </a>
+        <div class="mt-3 mb-10 text-xs text-center text-indigo-900 text-opacity-50">
+            Available for
+            @php
+                $expirationDate = \Carbon\Carbon::createFromFormat('Y-m-d H:i', '2024-11-30 00:00' );
+            @endphp
+            <x-countdown class="inline-block tabular-nums font-bold" :expires="$expirationDate">
+                <span x-text="timer.days">{{ $component->days() }}</span>d</span>
+                <span x-text="timer.hours">{{ $component->hours() }}</span>h</span>
+                <span x-text="timer.minutes">{{ $component->minutes() }}</span>m</span>
+                <span x-text="timer.seconds">{{ $component->seconds() }}</span>s</span>
+            </x-countdown>
+        </div>
+    @endif
 </div>
 
 
