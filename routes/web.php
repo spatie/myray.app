8e0deace3463908d8baaac99f425362ea7c12cfd
Jimi Robaer <jimi.robaer@hey.com>
2026-01-05 10:37:42 +0100

    Revert "Add temporary reroute to /teaser"
    This reverts commit 26e30e82a812e274b405d8a836fd3b41911fc244.

--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,35 @@
 <?php
 
-// use App\Http\Front\Controllers\DocsController;
-// use App\Http\Front\Controllers\HomeController;
-// use App\Http\Front\Controllers\PostsController;
+use App\Http\Front\Controllers\DocsController;
+use App\Http\Front\Controllers\HomeController;
+use App\Http\Front\Controllers\PostsController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'teaser.show')->name('teaser');
-Route::redirect('{any}', '/', 301)->where('any', '.*');
+Route::get('/', HomeController::class)->name('home');
+
+Route::redirect('wordpress', '/')->name('wordpress');
+Route::redirect('javascript', '/')->name('javascript');
 
 Route::name('legal.')->group(function () {
     Route::view('terms-of-use', 'legal.terms-of-use')->name('terms');
     Route::view('privacy', 'legal.privacy')->name('privacy');
 });
 
+Route::view('changelog', 'changelog.show')->name('changelog');
+Route::view('teaser', 'teaser.show')->name('teaser');
+
 Route::feeds();
 
-// Route::view('changelog', 'changelog.show')->name('changelog');
-// Route::view('teaser', 'teaser.show')->name('teaser');
+Route::prefix('blog')->name('blog.')->group(function () {
+    Route::get('/', [PostsController::class, 'index'])->name('index');
+    Route::get('/{slug}', [PostsController::class, 'detail'])->name('show');
+});
 
-// Route::prefix('blog')->name('blog.')->group(function () {
-//     Route::get('/', [PostsController::class, 'index'])->name('index');
-//     Route::get('/{slug}', [PostsController::class, 'detail'])->name('show');
-// });
+Route::get('login', fn() => redirect()->to('/admin/login'))->name('login');
 
-// Route::get('login', fn() => redirect()->to('/admin/login'))->name('login');
-
-// Route::prefix('docs')->name('docs.')->group(function () {
-//     Route::get('/', [DocsController::class, 'index'])->name('index');
-//     Route::get('/{slug}', [DocsController::class, 'show'])
-//         ->where('slug', '.*')
-//         ->name('show');
-// });
+Route::prefix('docs')->name('docs.')->group(function () {
+    Route::get('/', [DocsController::class, 'index'])->name('index');
+    Route::get('/{slug}', [DocsController::class, 'show'])
+        ->where('slug', '.*')
+        ->name('show');
+});
