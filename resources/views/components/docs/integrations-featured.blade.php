b515b38e6a45037182cf0e9f54bc53bede425c67
Jimi Robaer <jimi.robaer@hey.com>
2026-01-21 12:06:41 +0100

    Add missing logos to integrations

--- resources/views/components/docs/integrations-featured.blade.php
+++ resources/views/components/docs/integrations-featured.blade.php
@@ -1,18 +1,20 @@
-<div class="grid grid-cols-3 gap-8 my-8 lg:grid-cols-3">
-    @foreach ($docs as $doc)
-        <a wire:navigate href="{{ $doc->url }}" class="relative text-center group leading-tight">
-            <div
-                class="flex-1 rounded-2xl overflow-hidden mb-3 bg-gradient-to-b from-neutrals-white-20 to-red p-[1px] group-hover:from-orange group-hover:to-bright-orange">
+<div class="pb-4 border-b border-bleak-purple-extra-light/20">
+    <div class="grid grid-cols-3 gap-8 my-8 lg:grid-cols-3">
+        @foreach ($docs as $doc)
+            <a wire:navigate href="{{ $doc->url }}" class="relative text-center group leading-tight">
                 <div
-                    class="flex flex-col align-items-center justify-center items-center py-4 rounded-2xl bg-bleak-purple bg-opacity-75 group-hover:bg-opacity-100">
-                    <img class="w-full p-2 max-w-16 md:max-w-24" src="/images/logos/icons/logo-{{ $doc->parts[1] }}.svg"
-                        alt="{{ $doc->title }}">
+                    class="flex-1 rounded-2xl overflow-hidden mb-3 bg-gradient-to-b from-neutrals-white-20 to-red p-[1px] group-hover:from-orange group-hover:to-bright-orange">
+                    <div
+                        class="flex flex-col align-items-center justify-center items-center py-4 rounded-2xl bg-bleak-purple bg-opacity-75 group-hover:bg-opacity-100">
+                        <img class="w-full p-2 max-w-16 md:max-w-24" src="/images/logos/icons/logo-{{ $doc->parts[1] }}.svg"
+                            alt="{{ $doc->title }}">
+                    </div>
                 </div>
-            </div>
-            <span class="text-white font-semibold inline-block">{{ $doc->title }}</span>
-        </a>
-    @endforeach
+                <span class="text-white font-semibold inline-block">{{ $doc->title }}</span>
+            </a>
+        @endforeach
+    </div>
+    <p class="text-center">
+        <a wire:navigate href="/docs/getting-started/integrations" class="text-orange hover:underline">View all integrations →</a>
+    </p>
 </div>
-<p class="text-center">
-    <a wire:navigate href="/docs/getting-started/integrations" class="text-orange hover:underline">View all integrations →</a>
-</p>
